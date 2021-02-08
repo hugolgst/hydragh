71a03aef5c3571805f5556eff8b1350203944e8d
Hugo Lageneste <hugo.lageneste@visium.ch>
2021-02-08 23:09:49 +0100

    Write a helper function to create status on commits

--- main.go
+++ main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/v33/github"
-	"github.com/gorilla/mux"
 )
 
 // String is a helper routine that allocates a new string value
@@ -25,64 +20,88 @@ var (
 	itr            *ghinstallation.Transport
 )
 
-func processPR(ID string) {
-	atr, err := ghinstallation.NewAppsTransportKeyFromFile(http.DefaultTransport, appID, certPath)
-	if err != nil {
-		log.Fatal("error creating GitHub app client")
-	}
+// Contains all the states descriptions
+var descriptions = map[string]string{
+	"success": "All the jobs were successful.",
+	"pending": "Currently building the jobs...",
+	"failure": "At least one of the jobs failed.",
+	"error":   "Error",
+}
 
-	installation, _, err := github.NewClient(&http.Client{Transport: atr}).Apps.FindUserInstallation(context.TODO(), "hugolgst")
-	if err != nil {
-		log.Fatalf("error finding organization installation: %v", err)
-	}
-
-	installationID = installation.GetID()
-	itr = ghinstallation.NewFromAppsTransport(atr, installationID)
-
-	client := github.NewClient(&http.Client{Transport: itr})
-
+// WriteStatus creates a new status via the provided client on a specific commit of a client's repository
+// The description is automatically written and the target URL is hard-coded.
+func WriteStatus(client *github.Client, owner, repo, rev, state, jobName string) {
 	client.Repositories.CreateStatus(
 		context.TODO(),
-		"hugolgst",
-		"github-hydra-bot",
-		ID,
+		owner,
+		repo,
+		rev,
 		&github.RepoStatus{
-			State:       String("success"),
-			Description: String("test"),
+			State:       String(state),
+			Description: String(descriptions[state]),
+			TargetURL:   String("https://hydra.visium.ch"),
+			Context:     String(jobName),
 		},
 	)
 }
 
-func eventHandler(w http.ResponseWriter, r *http.Request) {
-	payload, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("error reading request body: err=%s\n", err)
-		return
-	}
-	defer r.Body.Close()
+// func processPR(ID string) {
+// 	atr, err := ghinstallation.NewAppsTransportKeyFromFile(http.DefaultTransport, appID, certPath)
+// 	if err != nil {
+// 		log.Fatal("error creating GitHub app client")
+// 	}
 
-	event, err := github.ParseWebHook(github.WebHookType(r), payload)
-	if err != nil {
-		log.Printf("could not parse webhook: err=%s\n", err)
-		return
-	}
+// 	installation, _, err := github.NewClient(&http.Client{Transport: atr}).Apps.FindUserInstallation(context.TODO(), "hugolgst")
+// 	if err != nil {
+// 		log.Fatalf("error finding organization installation: %v", err)
+// 	}
 
-	switch event := event.(type) {
-	case *github.PushEvent:
-		processPR(*event.Commits[0].ID)
-	default:
-		fmt.Println(event)
-		return
-	}
-}
+// 	installationID = installation.GetID()
+// 	itr = ghinstallation.NewFromAppsTransport(atr, installationID)
 
-func main() {
-	router := mux.NewRouter()
+// 	client := github.NewClient(&http.Client{Transport: itr})
 
-	router.HandleFunc("/event_handler", eventHandler).Methods("POST")
+// 	WriteStatus(
+// 		client,
+// 		"hugolgst",
+// 		"github-hydra-bot",
+// 		"6193b434b0bb87427a267785e8cfbc6e3f90759e",
+// 		"failure",
+// 		"vinixos/overlay",
+// 	)
+// }
 
-	err := http.ListenAndServe(":10000", router)
-	if err != nil {
-		panic(err)
-	}
-}
+// func eventHandler(w http.ResponseWriter, r *http.Request) {
+// 	payload, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		log.Printf("error reading request body: err=%s\n", err)
+// 		return
+// 	}
+// 	defer r.Body.Close()
+
+// 	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+// 	if err != nil {
+// 		log.Printf("could not parse webhook: err=%s\n", err)
+// 		return
+// 	}
+
+// 	switch event := event.(type) {
+// 	case *github.PushEvent:
+// 		processPR(*event.Commits[0].ID)
+// 	default:
+// 		fmt.Println(event)
+// 		return
+// 	}
+// }
+
+// func main() {
+// 	processPR("s")
+// 	router := mux.NewRouter()
+
+// 	router.HandleFunc("/event_handler", eventHandler).Methods("POST")
+
+// 	err := http.ListenAndServe(":10000", router)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
